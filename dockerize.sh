e3187fa6a064b04f4c3535cd3a0007c4ba4423fb
Yaakov Neuman <yakinew@yahoo.com>
2025-05-10 23:52:05 +0300

    feat(google_api) : Let user to set his name and wife name
    Issue: YN-103
    
    Signed-off-by: Yaakov Neuman <yakinew@yahoo.com>

--- dockerize.sh
+++ dockerize.sh
@@ -4,7 +4,10 @@ export $(cat .env | xargs)
 
 docker buildx build --debug -t love_letter:v1 .
 
-docker run -e "GEMINI_API_KEY=$GEMINI_API_KEY" \
+docker run \
+           -e "MY_NAME=$MY_NAME" \
+           -e "WIFE_NAME=$WIFE_NAME" \
+           -e "GEMINI_API_KEY=$GEMINI_API_KEY" \
            -e "MY_EMAIL=$MY_EMAIL" \
            -e "MY_EMAIL_KEY=$MY_EMAIL_KEY" \
            -e "MY_WIFE_EMAIL=$MY_WIFE_EMAIL" \
